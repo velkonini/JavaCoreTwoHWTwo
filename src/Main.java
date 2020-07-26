91cd84a3866a29f33306954256ece0b71ce02a4d
velkonini <avelkov07@gmail.com>
2020-07-26 13:29:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void Array() throws MyArraySizeExeption {
+        String[][] arr = new String[4][4];
+
+        }
+
+}
---
+++ src/MyArraySizeExeption.java
@@ -0,0 +1,2 @@
+public class MyArraySizeExeption extends Exception {
+}
